b6827c05b3bc7c862f1c106df5b48c1093feecc1
mariano alvarez <mariano@codingco.io>
2023-10-10 13:58:11 -0300

    jasmine tobe toequal tobetrhuty tobefalsy

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,5 +1,21 @@
 const saludar = require("../app");
 
-it("La función saluda", () => {
-  expect(saludar("Platzi")).toBe("Hola Platzi");
+var x = true;
+var a = {};
+var b = {};
+
+describe("verificar que la variable es true", () => {
+  it("La función saluda", () => {
+    expect(saludar("Platzi")).toBe("Hola Platzi");
+  });
+  it("La función saluda", () => {
+    expect(saludar("Platzi")).toContain("Hola");
+  });
+  it("X es true", () => {
+    expect(x).toBeTruthy();
+    expect(x).toEqual(true);
+  });
+  it("objetos iguales", () => {
+    expect(a).toEqual(b);
+  });
 });
